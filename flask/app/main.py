220fb3c235a87a20fb6f085a839dab1c138b44ce
YUX <yu.xiao.fr@gmail.com>
2016-05-16 22:52:51 +0200

    fixed mp3 MIMETYPE for music

--- flask/app/main.py
+++ flask/app/main.py
@@ -8,25 +8,38 @@ app.debug = False
 CHUNK_SIZE = 2048
 
 def qWhiteList(url):
+    try:
+        postfix = re.findall(r'[.](jpg|gif|bmg|mp3|png)', url)[-1]
+    except:
+        abort(403)
     if re.match(r'ww[0-9]\.sinaimg.cn\/', url):
         site = "sinaimg"
     elif re.match(r'p[0-9]\.music.126.net\/', url):
         site = "163music"
     else:
          abort(403)
-    return site
+    return [site,postfix]
 
 @app.route('/<path:url>')
 def proxy(url):
-    site = qWhiteList(url)
+    site = qWhiteList(url)[0]
+    postfix = qWhiteList(url)[1]
     if site == "sinaimg":
         r = requests.get("http://"+url, stream=True)
+        headers = r.raw.headers.items()
     elif site == "163music":
         r = requests.get("http://"+url, headers={"Referer": "http://music.163.com/"}, stream=True)
+        headers = r.raw.headers.items()
+        if postfix == "mp3":
+            headers[-2] = ("Content-Type","audio/mpeg; charset=UTF-8")
+        else:
+            pass
+    else:
+        pass
     def generate():
         for chunk in r.iter_content(CHUNK_SIZE):
             yield chunk
-    return Response(generate(), headers = r.raw.headers.items())
+    return Response(generate(), headers = headers)
 
 @app.errorhandler(403)
 def forbidden(e):
