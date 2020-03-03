6661b88b56cd03f7f4aea1675c244dfda6ca1375
XIAO Yu <yu.xiao.fr@gmail.com>
2020-03-03 08:25:04 +0800

    init

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, Response, abort, send_from_directory
+from flask import Flask, Response, abort, send_from_directory, redirect
 from mimetypes import MimeTypes
 import requests, os
 
 app = Flask(__name__)
-app.debug = True
-CHUNK_SIZE = 2048
+app.debug = False
+CHUNK_SIZE = 1024*1024
 
 @app.route('/<path:url>')
 def proxy(url):
     r = requests.get(url, stream=True)
-    
     if r.status_code != 200:
         abort(r.status_code)
 
     mime = MimeTypes()
     mime_type = mime.guess_type(url)
+    headers = r.raw.headers.items()
     def generate():
         for chunk in r.iter_content(CHUNK_SIZE):
             yield chunk
-    return Response(generate(), mimetype=mime_type[0])
-
+    return Response(generate(),headers = headers, mimetype=mime_type[0])
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def index():
+    return redirect('https://github.com/YUX-IO/ffp', code=302)
 
 if __name__ == '__main__':
     app.run()
