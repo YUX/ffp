6c9ee777e33d247ee2addf3127a283aeac2efa72
XIAO Yu <yu.xiao.fr@gmail.com>
2020-03-04 18:49:48 +0800

    Update test_ffp.py

--- test_ffp.py
+++ test_ffp.py
@@ -3,6 +3,6 @@ class TestClass(object):
     def setup_class(self):
         self.app = app.test_client()
     def test_home_page(self):
-        rv = self.app.get('/')
-        assert rv.status_code == 302
+        rv = self.app.get('/https://raw.githubusercontent.com/YUX-IO/ffp/master/test')
+        assert rv.status_code == 200
         assert rv.data == 'ffp'
