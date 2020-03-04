44d2808c3b5b50b11e0abd3fa76fc65be5b87b22
XIAO Yu <yu.xiao.fr@gmail.com>
2020-03-04 21:15:53 +0800

    add coveralls

--- test_ffp.py
+++ test_ffp.py
@@ -9,4 +9,4 @@ class TestClass(object):
     def test_ffp_r(self):
         rv = self.app.get('/r/https://raw.githubusercontent.com/YUX-IO/ffp/master/test')
         assert rv.status_code == 200
-        assert rv.data == b'https://127.0.0.1:5000/https://github.com/YUX-IO/ffp'
+        assert rv.data == b'https://localhost/https://github.com/YUX-IO/ffp'
