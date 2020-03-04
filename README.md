17c082adbf33fe817db9a154b7f4ed4c6c8d0abb
XIAO Yu <yu.xiao.fr@gmail.com>
2020-03-04 15:24:05 +0800

    README

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
-# ffp
-yet another Flask File Proxy
+**# ffp**
 
-```
-pip install -r requirements.txt
-python main.py
-```
+**## yet another Flask File Proxy**
 
-```
+**### RECOMMENDED**
+
+**make sure you have**
+
+- docker, `docker --version`
+- Domain name
+- ssl
+- Nginx (or anything you can bind your domain to local port 502)
+
+### INSTALLATION
+
+1. install ffp via docker 
+
+\```shell
+
 docker run -d --name=ffp \
+
   -p 127.0.0.1:502:80 \
-  yuxio/ffp
-```
-handmade by [YUX](https://yux.io)
+
+  yuxio/ffp:latest
+
+\```
+
+2. Point your domain(or subdomain) to `127.0.0.1:502`
+
+3. Get the HTTPS work
+
+### USAGE
+
+1. **File Proxy**
+   - Rewrite `https://github.com/YUX-IO/ffp/blob/master/README.md` to `https://your.domain.here/https://github.com/YUX-IO/ffp/blob/master/README.md`
+   - Now you get the file passed through the proxy.
+
+2. **Rewritten Script Proxy**          --*please note `/r/` in the url*
+   - Rewrite `https://github.com/YUX-IO/ffp/blob/master/README.md` to `https://your.domain.here/r/https://github.com/YUX-IO/ffp/blob/master/README.md`
+   - This will replace all the URL in the script to `https://your.domain.here/URL`, which means you'd get all the resources passing through your proxy.
+
+handmade by [XIAO Yu](
--- main.py
+++ main.py
@@ -1,35 +1,52 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, Response, abort, send_from_directory, redirect
+from flask import Flask, Response, abort, send_from_directory, redirect, request
 from mimetypes import MimeTypes
-import requests, os
+import requests, os, re
 
 app = Flask(__name__)
 app.debug = False
 CHUNK_SIZE = 1024*1024
+mime = MimeTypes()
+
 
 @app.route('/<path:url>')
 def proxy(url):
     r = requests.get(url, stream=True)
+    responseHeaders=r.raw.headers.items()
     if r.status_code != 200:
         abort(r.status_code)
-
-    mime = MimeTypes()
     mime_type = mime.guess_type(url)
-    headers = r.raw.headers.items()
     def generate():
         for chunk in r.iter_content(CHUNK_SIZE):
             yield chunk
-    return Response(generate(),headers = headers, mimetype=mime_type[0])
+    return Response(generate(),headers = responseHeaders, mimetype=mime_type[0])
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+@app.route('/r/<path:url>')
+def replace(url):
+    r = requests.get(url)
+    responseHeaders=r.raw.headers.items()
+    if r.status_code != 200:
+        abort(r.status_code)
+    mime_type = mime.guess_type(url)
+    requestHeaders=request.headers
+    host=requestHeaders["Host"]
+    postRegex=re.sub(r"http","https://"+host+"/http",r.text)
+
+    return Response(postRegex,headers=responseHeaders, mimetype=mime_type[0])
 
 @app.route('/')
 def index():
     return redirect('https://github.com/YUX-IO/ffp', code=302)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
 @app.errorhandler(500)
+def internal_server_rror(e):
+	return redirect('https://github.com/YUX-IO/ffp', code=302)
+
+@app.errorhandler(400)
 def internal_server_rror(e):
 	return redirect('https://github.com/YUX-IO/ffp', code=302)
 
