0ebd497c552a78b53bc2bb0ac44359aa1959ec13
XIAO Yu <yu.xiao.fr@gmail.com>
2020-03-16 21:30:03 +0800

    mv to pyCharm

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, Response, abort, send_from_directory, redirect, request
+import os
+import re
 from mimetypes import MimeTypes
-import requests, os, re
+
+import requests
+from flask import Flask, Response, abort, send_from_directory, redirect, request
 
 app = Flask(__name__)
 app.debug = False
 mime = MimeTypes()
-CHUNK_SIZE = 2*1024*1024
+CHUNK_SIZE = 2 * 1024 * 1024
+
 
 @app.route('/<path:url>')
 def proxy(url):
-    requestHeaders=dict(request.headers)
-    #hostPattern=re.compile(r"\/\/(.*?)\/")
-    #requestHost=hostPattern.findall(url)[0]
-    #requestHeaders['Host']=requestHost
+    requestHeaders = dict(request.headers)
+    # hostPattern=re.compile(r"\/\/(.*?)\/")
+    # requestHost=hostPattern.findall(url)[0]
+    # requestHeaders['Host']=requestHost
     try:
         requestHeaders.pop('If-None-Match')
     except:
@@ -26,26 +30,29 @@ def proxy(url):
         requestHeaders.pop('Host')
     except:
         pass
-    r = requests.get(url, stream=True,params=request.args,headers=requestHeaders)
+    r = requests.get(url, stream=True, params=request.args, headers=requestHeaders)
     if r.status_code != 200:
         abort(r.status_code)
     mime_type = mime.guess_type(url)
+
     def generate():
         for chunk in r.iter_content(CHUNK_SIZE):
             yield chunk
-    responseHeaders=dict(r.headers)
+
+    responseHeaders = dict(r.headers)
     try:
         responseHeaders.pop('Transfer-Encoding')
     except:
         pass
-    return Response(generate(),headers=responseHeaders,mimetype=mime_type[0])
+    return Response(generate(), headers=responseHeaders, mimetype=mime_type[0])
+
 
 @app.route('/r/<path:url>')
 def replace(url):
-    requestHeaders=dict(request.headers)
-    #hostPattern=re.compile(r"\/\/(.*?)\/")
-    #requestHost=hostPattern.findall(url)[0]
-    #requestHeaders['Host']=requestHost
+    requestHeaders = dict(request.headers)
+    # hostPattern=re.compile(r"\/\/(.*?)\/")
+    # requestHost=hostPattern.findall(url)[0]
+    # requestHeaders['Host']=requestHost
     try:
         requestHeaders.pop('If-None-Match')
     except:
@@ -58,35 +65,41 @@ def replace(url):
         requestHeaders.pop('Host')
     except:
         pass
-    r = requests.get(url,params=request.args,headers=requestHeaders)
+    r = requests.get(url, params=request.args, headers=requestHeaders)
     if r.status_code != 200:
         abort(r.status_code)
     mime_type = mime.guess_type(url)
-    localHost=request.headers["Host"]
+    localHost = request.headers["Host"]
     pattern = re.compile(r'http(s:\/\/|:\/\/)')
-    postRegex=re.sub(pattern,"https://"+localHost+"/http"+r'\1', r.text)
-    responseHeaders=dict(r.headers)
+    postRegex = re.sub(pattern, "https://" + localHost + "/http" + r'\1', r.text)
+    responseHeaders = dict(r.headers)
     try:
         responseHeaders.pop('Content-Encoding')
     except:
         pass
-    return Response(postRegex,headers=responseHeaders, mimetype=mime_type[0])
+    return Response(postRegex, headers=responseHeaders, mimetype=mime_type[0])
+
 
 @app.route('/')
 def index():
     return redirect('https://github.com/YUX-IO/ffp', code=302)
 
+
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return redirect('https://github.com/YUX-IO/ffp', code=302)
+    return redirect('https://github.com/YUX-IO/ffp', code=302)
+
 
 @app.errorhandler(400)
 def bad_equest(e):
-	return redirect('https://github.com/YUX-IO/ffp', code=302)
+    return redirect('https://github.com/YUX-IO/ffp', code=302)
+
 
 if __name__ == '__main__':
     app.run()
